f0d9d059f149f56b88a92f8168228fe4c74fbe7b
DiegoZapana <dzapanac@ulasalle.edu.pe>
2026-06-28 14:09:32 -0500

    Juntando el trabajo de Carlitos que lo hizo bien

---
+++ js/controllers/AudioController.js
@@ -0,0 +1,44 @@
+/**
+ * controllers/AudioController.js
+ * Es para controlar el tipo de sonido que empezarà a sonar cada sierto tiempo
+ */
+
+export class AudioController{
+
+    constructor(){
+        this.bgmPrimary = new Audio('assets/audio/seg.mp3');
+        this.bgmSecondary = new Audio('assets/audio/prim.mp3');
+
+        this.bgmPrimary.loop = true;
+        this.bgmSecondary.loop = true;
+
+        this.bgmPrimary.volume = 0.45;
+        this.bgmSecondary.volume = 0.45;
+
+        this.currentTrack = null;
+    }
+
+    playPrimary(){
+        this.stopCurrent();
+        this.currentTrack = this.bgmPrimary;
+        this.currentTrack.play().catch(e => console.warn("El navegador bloqueo el audtoplay: ", e));
+    }
+
+    playSecondary(){
+        this.stopCurrent();
+        this.currentTrack = this.bgmSecondary;
+        this.currentTrack.play().catch(e => console.warn("El navegoadro bloque el autoplay: ", e));
+    }
+
+    stopCurrent(){
+        if(this.currentTrack){
+            this.currentTrack.pause();
+            this.currentTrack.currentTime = 0;
+        }
+    }
+
+    stopAll(){
+        this.stopCurrent();
+        this.currentTrack = null;
+    }
+}
